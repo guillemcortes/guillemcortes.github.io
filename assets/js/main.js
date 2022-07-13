64d7657cb3718413fa5d96dcec6e3320d26ba100
Guillem Cortès <gcortes@bmat.com>
2022-07-13 20:03:47 +0200

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,7 @@ skillsHeader.forEach((el) =>{
   el.addEventListener('click', toggleSkills)
 })
 
-/*==================== QUALIFICATION TABS ====================*/
+/*==================== path TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
 
@@ -58,14 +58,14 @@ tabs.forEach(tab =>{
     const target = document.querySelector(tab.dataset.target)
 
     tabContents.forEach(tabContent =>{
-      tabContent.classList.remove('qualification__active')
+      tabContent.classList.remove('path__active')
     })
-    target.classList.add('qualification__active')
+    target.classList.add('path__active')
 
     tabs.forEach(tab =>{
-      tab.classList.remove('qualification__active')
+      tab.classList.remove('path__active')
     })
-    tab.classList.add('qualification__active')
+    tab.classList.add('path__active')
   })
 })
 
@@ -84,8 +84,26 @@ const paragraph = `
 `;
 
 document.getElementById('copyright').innerHTML = paragraph;
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
